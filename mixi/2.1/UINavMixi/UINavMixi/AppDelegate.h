0cab700ca083b8733de538fd2e1f89eecb110a7a
timakin <timaki.st@gmail.com>
2015-03-18 21:25:08 +0900

    UINavMixi added

---
+++ mixi/2.1/UINavMixi/UINavMixi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UINavMixi
+//
+//  Created by 高橋 誠二 on 2015/03/18.
+//  Copyright (c) 2015年 高橋 誠二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mixi/2.1/UINavMixi/UINavMixi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UINavMixi
+//
+//  Created by 高橋 誠二 on 2015/03/18.
+//  Copyright (c) 2015年 高橋 誠二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
